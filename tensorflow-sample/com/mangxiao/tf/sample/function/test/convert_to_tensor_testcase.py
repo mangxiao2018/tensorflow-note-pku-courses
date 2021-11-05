4927b36305dadd7ba7361ad93c11a20957815549
mangxiao <mangxiao2018@126.com>
2021-11-05 19:15:30 +0800

    random、numpy转张量、创建张量zeros/ones/fill类型的

--- tensorflow-sample/com/mangxiao/tf/sample/function/test/convert_to_tensor_testcase.py
+++ tensorflow-sample/com/mangxiao/tf/sample/function/test/convert_to_tensor_testcase.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 a = np.arange(0, 5)
+# 把numpy数组转成张量
 b = tf.convert_to_tensor(a, dtype=tf.int64)
 
 print("a:",a)
---
+++ tensorflow-sample/com/mangxiao/tf/sample/function/test/zeros_ones_fill_testcase.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+a = tf.zeros([2,3])
+b = tf.ones([2,3])
+c = tf.fill([2,3],9)
+
+print(a)
+print(b)
+print(c)
